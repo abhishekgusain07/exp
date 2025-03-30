a60741da9608e505dfc062816eb4a78e126f38ff
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-03-30 17:45:10 +0530

    remove next/font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./context/auth-context";
 import AppWrapper from "./app-wrapper";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "MoneyWhisper",
@@ -26,8 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <body className="antialiased"
       >
         <AuthProvider>
           <AppWrapper>{children}</AppWrapper>
